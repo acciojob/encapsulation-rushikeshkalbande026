5cc194f39e8021d994f04221013c6d875f3ce1e4
Rushikesh <rushikeshkalbande026@gmail.com>
2023-05-01 19:53:25 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main (String[] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("Rushi");
+        //obj.getName();
+        System.out.println(obj.getName());
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
-
 public class RWOnly {
-    
+    //Zxc vn,.21q AzSW32z32qazf+-
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        this.name = newName;
+    }
 }
+
+
